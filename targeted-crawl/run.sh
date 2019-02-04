a056f4fd72ac30c088eda783612d855c4cf0c7a3
Hieu Hoang <hieuhoang@gmail.com>
2019-02-04 17:39:01 +0000

    run mysql

--- targeted-crawl/run.sh
+++ targeted-crawl/run.sh
@@ -1,8 +1,7 @@
 
 
-#xzcat www.samsonite.be.xz | ./import-mysql.py --out-dir out
+xzcat www.samsonite.be.xz | ./import-mysql.py --out-dir out
 #xzcat www.bizerba.com.xz | ./import-mysql.py --out-dir out
 
-BITEXTOR=/home/hieu/workspace/github/paracrawl/bitextor.malign
-
-xzcat www.samsonite.be.xz | $BITEXTOR/bitextor-warc2preprocess.py --output-dir out --lang1 en --lang2 fr
+#BITEXTOR=/home/hieu/workspace/github/paracrawl/bitextor.malign
+#xzcat www.samsonite.be.xz | $BITEXTOR/bitextor-warc2preprocess.py --output-dir out --lang1 en --lang2 fr
