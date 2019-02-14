03f9d913e253b3beefd41d085c1a59ee2813f0ce
Hieu Hoang <hieuhoang@gmail.com>
2019-02-14 17:40:44 +0000

    pipes to translate

--- targeted-crawl/translate.py
+++ targeted-crawl/translate.py
@@ -5,6 +5,16 @@ import sys
 import argparse
 import mysql.connector
 import lzma
+import socket
+import subprocess
+
+###########################################################
+def systemCheck(cmd):
+    sys.stderr.write("Executing:" + cmd + " on " + socket.gethostname() + "\n")
+    sys.stderr.flush()
+
+    subprocess.check_call(cmd, shell=True)
+###########################################################
 
 oparser = argparse.ArgumentParser(description="Hello")
 oparser.add_argument('--dir', dest='dir', help='Data directory', required=True)
@@ -27,7 +37,7 @@ for fileName in os.listdir(options.dir):
     filePrefix = fileName[:-8]
     fileId = int(filePrefix)
     translatedFile = filePrefix + ".trans"
-    print("fileName", fileName, filePrefix, fileName[-8:], translatedFile)
+    #print("fileName", fileName, filePrefix, fileName[-8:], translatedFile)
 
     # translated already?
     if os.path.isfile(translatedFile):
@@ -40,4 +50,29 @@ for fileName in os.listdir(options.dir):
     res = mycursor.fetchone()
     assert(res != None)
     lang = res[0]
-    print("lang", lang)
+    print("fileName", fileName, filePrefix, fileName[-8:], translatedFile, lang)
+
+    if lang != "fr":
+        continue
+
+    txtPath = options.dir + "/" + fileName
+    with lzma.open(txtPath, 'rt') as f:
+        txt = f.read()
+    #print("txt", txt)
+
+    #cmd = "xzcat " + txtPath + " | ~/workspace/github/mosesdecoder/bin/moses2 -f /home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
+    #systemCheck(cmd)
+    proc = subprocess.Popen(["/home/hieu/workspace/github/mosesdecoder/bin/moses2",
+                             "-f",
+                             "/home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"],
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    proc.stdin.write("matthew\n".encode('utf-8'))
+    proc.stdin.write("mark\n".encode('utf-8'))
+    proc.stdin.write("luke\n".encode('utf-8'))
+    proc.stdin.close()
+
+    while proc.returncode is None:
+        proc.poll()
+
+    print("I got back from the program this:\n{0}".format(proc.stdout.read()))
+
