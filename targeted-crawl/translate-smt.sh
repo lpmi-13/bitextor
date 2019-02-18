21e80c7d889843a3210f2354d0c25c65be5804ae
Hieu Hoang <hieuhoang@gmail.com>
2019-02-18 17:27:02 +0000

    disable buffering

--- targeted-crawl/translate-smt.sh
+++ targeted-crawl/translate-smt.sh
@@ -6,7 +6,7 @@ MOSES_MODEL=$3
 
 
 cat /dev/stdin |
-  $MOSES/scripts/tokenizer/tokenizer.perl -a -l $LANG1 |
+  $MOSES/scripts/tokenizer/tokenizer.perl -a -b -l $LANG1 |
   $MOSES/bin/moses2 -f $MOSES_MODEL --threads 16 -v 0 -search-algorithm 1 -cube-pruning-pop-limit 1000 |
-  $MOSES/scripts/tokenizer/detokenizer.perl
+  $MOSES/scripts/tokenizer/detokenizer.perl -b
 
