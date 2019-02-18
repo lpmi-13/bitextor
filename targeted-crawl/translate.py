9457c09460ea786d564c985ea7770a0aae86eeb4
Hieu Hoang <hieuhoang@gmail.com>
2019-02-18 18:24:14 +0000

    reuse moses process

--- targeted-crawl/translate.py
+++ targeted-crawl/translate.py
@@ -30,6 +30,15 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+# cmd = "xzcat " + txtPath + " | ~/workspace/github/mosesdecoder/bin/moses2 -f /home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
+# systemCheck(cmd)
+proc = subprocess.Popen(["/home/hieu/workspace/github/paracrawl/bitextor.hieu.malign/targeted-crawl/translate-smt.sh",
+                         "fr",
+                         "/home/hieu/workspace/github/mosesdecoder",
+                         "/home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
+                         ],
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
 for fileName in os.listdir(options.dir):
     if fileName[-8:] != ".text.xz":
         continue
@@ -46,12 +55,11 @@ for fileName in os.listdir(options.dir):
     # what language is it in?
     sql = "SELECT lang FROM document WHERE id = %s"
     val = (fileId,)
-    print("fileId", fileId)
+    #print("fileId", fileId)
     mycursor.execute(sql, val)
     res = mycursor.fetchone()
     assert(res != None)
     lang = res[0]
-    print("fileName", fileName, filePrefix, fileName[-8:], translatedFile, lang)
 
     if lang != "fr":
         continue
@@ -62,17 +70,9 @@ for fileName in os.listdir(options.dir):
         inLines = inLines.split("\n")
         if len(inLines[-1]) == 0:
             del inLines[-1]
-    print("inLines", len(inLines), inLines[-1])
+    print("fileName", fileName, filePrefix, fileName[-8:], translatedFile, lang, len(inLines))
 
-    #cmd = "xzcat " + txtPath + " | ~/workspace/github/mosesdecoder/bin/moses2 -f /home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
-    #systemCheck(cmd)
-    proc = subprocess.Popen(["/home/hieu/workspace/github/paracrawl/bitextor.hieu.malign/targeted-crawl/translate-smt.sh",
-                             "fr",
-                             "/home/hieu/workspace/github/mosesdecoder",
-                             "/home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
-                             ],
-                            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
+    # translate
     transPath = options.dir + "/" + str(fileId) + ".trans.xz"
     outFile = lzma.open(transPath, 'wt')
 
