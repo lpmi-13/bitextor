f9dd756a7e1ceb832759c882c581463baba44215
Hieu Hoang <hieuhoang@gmail.com>
2019-02-19 17:20:33 +0000

    start sentence split

--- targeted-crawl/translate.py
+++ targeted-crawl/translate.py
@@ -32,12 +32,22 @@ mycursor = mydb.cursor()
 
 # cmd = "xzcat " + txtPath + " | ~/workspace/github/mosesdecoder/bin/moses2 -f /home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
 # systemCheck(cmd)
-proc = subprocess.Popen(["/home/hieu/workspace/github/paracrawl/bitextor.hieu.malign/targeted-crawl/translate-smt.sh",
+mtProc = subprocess.Popen(["/home/hieu/workspace/github/paracrawl/bitextor.hieu.malign/targeted-crawl/translate-smt.sh",
                          "fr",
                          "/home/hieu/workspace/github/mosesdecoder",
                          "/home/hieu/workspace/experiment/issues/paracrawl/fr-en/smt-dir/model/moses.bin.ini.1"
                          ],
                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+enSplitProc = subprocess.Popen(["/home/hieu/workspace/github/mosesdecoder/scripts/ems/support/split-sentences.perl",
+                                "-b",
+                                "-l", "en"],
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+frSplitProc = subprocess.Popen(["/home/hieu/workspace/github/mosesdecoder/scripts/ems/support/split-sentences.perl",
+                                "-b",
+                                "-l", "fr"],
+                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 for fileName in os.listdir(options.dir):
     if fileName[-8:] != ".text.xz":
@@ -72,18 +82,35 @@ for fileName in os.listdir(options.dir):
             del inLines[-1]
     print("fileName", fileName, filePrefix, fileName[-8:], translatedFile, lang, len(inLines))
 
-    # translate
+    # sentence split and translate
     transPath = options.dir + "/" + str(fileId) + ".trans.xz"
     outFile = lzma.open(transPath, 'wt')
 
     for inLine in inLines:
+        print("inLine", inLine)
         inLine += "\n"
-        proc.stdin.write(inLine.encode('utf-8'))
-        proc.stdin.flush()
-        outLine = proc.stdout.readline()
+
+        cout, cerr = frSplitProc.communicate(input=inLine.encode('utf-8'))
+
+        #frSplitProc.stdin.write(inLine.encode('utf-8'))
+        #frSplitProc.stdin.flush()
+
+        print("cout", cout)
+
+        #lines = frSplitProc.stdout.read()
+        #print("split", lines)
+        #while frSplitProc.poll() is None:
+        #    pass
+
+        #    line = frSplitProc.stdout.readline()
+        #    print("split", line)
+
+        mtProc.stdin.write(inLine.encode('utf-8'))
+        mtProc.stdin.flush()
+        outLine = mtProc.stdout.readline()
         outLine = outLine.decode("utf-8")
         outFile.write(outLine)
 
     outFile.close()
 
-
+    exit()
